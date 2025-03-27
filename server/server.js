fff56fb2075471a5f193a6638ce27e91c910a7ba
pankaj8920 <158765718+pankaj8920@users.noreply.github.com>
2025-03-27 09:53:10 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ dotenv.config(); // Load .env file
 import express from "express";
 import cors from "cors";
 import connectDB from "./configs/mongodb.js";
-import { clerkWebhooks } from "./controllers/webhooks.js";
+// import { clerkWebhooks } from "./controllers/webhooks.js";
 // import educatorRouter from "./routes/educatorRoutes.js";
 import { clerkMiddleware } from "@clerk/express";
 
@@ -13,15 +13,20 @@ const app = express();
 // Middlewares
 app.use(cors());
 app.use(express.json()); // Parse JSON bodies
-app.use(clerkMiddleware());
+// app.use(clerkMiddleware());
 
 // Connect to MongoDB
 await connectDB();
 
 // Routes
 app.get("/", (req, res) => res.send("API Working"));
-app.post('/clerk', clerkWebhooks); // Clerk webhook route
+// app.post('/clerk', clerkWebhooks); // Clerk webhook route
 // app.use('/api/educator', educatorRouter); // Educator routes
-
+// const PORT = process.env.PORT || 3000;
+// if (process.env.NODE_ENV !== "production") {
+//     app.listen(PORT, () => {
+//         console.log(`Server is running on port ${PORT}`);
+//     });
+// }
 // Export the Express app as a Vercel serverless function
 export default app;
